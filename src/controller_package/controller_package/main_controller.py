a97d7789211b2304a222415f9a89c53349cb5322
stasim-sudo <stasim49@gmail.com>
2026-04-29 15:24:22 +0100

    package xml update and main controller service clients added

--- src/controller_package/controller_package/main_controller.py
+++ src/controller_package/controller_package/main_controller.py
@@ -7,153 +7,173 @@ from rclpy.action.server import ServerGoalHandle
 
 from controller_interfaces import TurnExploreOn
 from controller_interfaces import TurnExploreOff
-
+from controller_interfaces import ControllerSet
+from controller_interfaces import ControllerPositionSet
 
 class ControllerNode(Node):
+    
     def __init__(self):
-        super().__init__("leo_controller")
-        self.current_case = 0
+        super().__init__('leo_controller')
+        self.current_case=0
 
-        self.starting_pos = None
+        self.starting_pos=None
 
         """ self.task_space_pose_subscriber = self.create_subscription(
             msg_type = TaskSpacePose,
             topic='/omega_operator/task_space_pose',
             qos_profile=1,
             callback=self.task_space_pose_subscriber_callback)"""
+    
 
-        # TF pose listener
+        #TF pose listener
         self.transform_listener_buffer = tf2_ros.Buffer()
-        self.tf_listener = tf2_ros.TransformListener(
-            self.transform_listener_buffer, self
-        )
-        self.parent_name = "world"
-        self.child_name = "leo1/base_link"
-        # initiates the turn on service linking callback
-
+        self.tf_listener = tf2_ros.TransformListener(self.transform_listener_buffer, self)
+        self.parent_name="world"
+        self.child_name="leo1/base_link"
+         
+        #Initialise the 
         self.service_client_robot_explore_on = self.create_client(
-            srv_type=TurnExploreOn, srv_name="/turn_exploration_on"
-        )
+             srv_type=TurnExploreOn,
+             srv_name='/turn_explore_on')
+        
+        self.service_client_robot_explore_off = self.create_client(
+             srv_type=TurnExploreOff,
+             srv_name='/turn_explore_off')
+        
+        self.service_client_controller_set = self.create_client(
+            srv_type=ControllerSet,
+            srv_name='/controller_set')
+        
+        self.service_client_controller_position_set = self.create_client(
+            srv_type=ControllerPositionSet,
+            srv_name='/controller_position_set')
+        
+        
+        #Wait for service servers to come online
+        while not self.service_client_robot_explore_on.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info(f'service {self.service_client_robot_explore_on.srv_name} not available, waiting...')
+        while not self.service_client_robot_explore_off.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info(f'service {self.service_client_robot__explore_off.srv_name} not available, waiting...')
+        while not self.service_client_controller_set.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info(f'service {self.service_client_robot_controller_set.srv_name} not available, waiting...')
+        while not self.service_client_controller_position_set.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info(f'service {self.service_client_controller_position_set.srv_name} not available, waiting...')
 
-        self.service_client_robot_explore_on = self.create_client(
-            srv_type=TurnExploreOff, srv_name="/turn_exploration_off"
-        )
+        
 
-        timer_period: float = 1
-        self.timer = self.create_timer(
-            timer_period_sec=timer_period, callback=self.main_loop_callback
-        )
+    
 
     def main_loop_callback(self):
         match self.current_case:
             case 0:
-                # get the starting position before moving onto anything else
-                loop_till_found = True
-                while loop_till_found:
-                    try:
-                        tfs = self.transform_listener_buffer.lookup_transform(
-                            self.parent_name, self.child_name, rclpy.time.Time()
-                        )
-                        self.starting_pos = tfs
-                        self.get_logger().info(f"Starting Pose: {tfs}")
-                        loop_till_found = False
+                #get the starting position before moving onto anything else
 
-                    except tf2_ros.TransformException as e:
+                
+                try:
+                        tfs =   self.transform_listener_buffer.lookup_transform(
+                                self.parent_name,
+                                self.child_name,
+                                rclpy.time.Time())
+                        self.starting_pos=tfs
+                        self.get_logger().info(f"Starting Pose: {tfs}")
+                       
+
+                except tf2_ros.TransformException as e:
+
                         self.get_logger().error(
-                            f"Could not get transform from `{self.parent_name}` to `{self.child_name}`: {e}"
-                        )
-                self.current_case = 1
+                            f'Could not get transform from `{self.parent_name}` to `{self.child_name}`: {e}')
+                self.current_case=1
+
 
             case 1:
+                #start exploration
+                #manipulator switches between far scan 0 and 1
+                #object detection returns 
                 pass
-                # start exploration
-                # manipulator switches between far scan 0 and 1
-                # object detection returns
 
+
+           
     def which_move_control(self):
-        self.time_for_rotation = 45
-        self.time_for_linear = 45  # must be at least distance*10
-        if self.is_robot_on == True:
-            if self.time_increment < (self.time_for_rotation * 10):
+        self.time_for_rotation=45
+        self.time_for_linear=45#must be at least distance*10
+        if self.is_robot_on==True:
+            if self.time_increment<(self.time_for_rotation*10):
                 self.rotation_control()
-            elif self.time_increment < (
-                (self.time_for_linear + self.time_for_rotation) * 10
-            ):
+            elif self.time_increment<((self.time_for_linear+self.time_for_rotation)*10):
                 self.linear_control()
             else:
                 self.robot_turn_off()
-                self.get_logger().info("Robot movement complete switching off")
-                self.is_robot_on = False
-
+                self.get_logger().info('Robot movement complete switching off')
+                self.is_robot_on=False
+                
+        
+        
+        
     def rotation_control(self):
+        
+        angle:float=-69
+        angle_rad=np.deg2rad(angle)
+        update_frequency=0.1
+        
+        anglular_velocity=((self.wheelbase*angle_rad)/(2*self.wheel_radius*self.time_for_rotation))
 
-        angle: float = -69
-        angle_rad = np.deg2rad(angle)
-        update_frequency = 0.1
-
-        anglular_velocity = (self.wheelbase * angle_rad) / (
-            2 * self.wheel_radius * self.time_for_rotation
-        )
-
-        wheel_velocities_rotate = WheelAngularVelocities()
-        wheel_velocities_rotate.left_wheel_angular_velocity = -anglular_velocity
-        wheel_velocities_rotate.right_wheel_angular_velocity = anglular_velocity
+        wheel_velocities_rotate=WheelAngularVelocities()
+        wheel_velocities_rotate.left_wheel_angular_velocity=-anglular_velocity
+        wheel_velocities_rotate.right_wheel_angular_velocity=anglular_velocity
         self.wheel_velocity_publisher.publish(wheel_velocities_rotate)
-        self.time_increment += 1
-        # self.get_logger().info(f'current time that you should know of is:{self.time_increment/10}')
+        self.time_increment+=1
+        #self.get_logger().info(f'current time that you should know of is:{self.time_increment/10}')
+        
+        
 
     def linear_control(self):
-
-        distance = 1
-
-        angular_velocity = distance / (self.wheel_radius * self.time_for_linear)
-        wheel_velocities_rotate = WheelAngularVelocities()
-        wheel_velocities_rotate.left_wheel_angular_velocity = angular_velocity
-        wheel_velocities_rotate.right_wheel_angular_velocity = angular_velocity
+        
+        distance=1
+        
+        angular_velocity=distance/(self.wheel_radius*self.time_for_linear)
+        wheel_velocities_rotate=WheelAngularVelocities()
+        wheel_velocities_rotate.left_wheel_angular_velocity=angular_velocity
+        wheel_velocities_rotate.right_wheel_angular_velocity=angular_velocity
         self.wheel_velocity_publisher.publish(wheel_velocities_rotate)
-        self.time_increment += 1
-        # self.get_logger().info(f'current time that you should know of is:{self.time_increment/10}')
-
+        self.time_increment+=1
+        #self.get_logger().info(f'current time that you should know of is:{self.time_increment/10}')
+       
+        
     def robot_turn_off(self):
         while not self.service_client_robot_off.wait_for_service(timeout_sec=1):
-            self.get_logger().info(
-                f"{self.service_client_robot_off.srv_name} service is not yet available"
-            )
+            self.get_logger().info(f'{self.service_client_robot_off.srv_name} service is not yet available')
         self.future_off: Future = None
-
-        request_off = TurnRobotOff.Request()
+      
+        request_off=TurnRobotOff.Request()
         if self.future_off is not None and not self.future_off.done():
             self.future_off.cancel()
-            self.get_logger().warn("Turn on cancelled is operator node still running? ")
-
-        self.future_off = self.service_client_robot_off.call_async(request_off)
+            self.get_logger().warn('Turn on cancelled is operator node still running? ')
+            
+            
+        self.future_off=self.service_client_robot_off.call_async(request_off)
         self.future_off.add_done_callback(self.after_robot_off)
 
-    def after_robot_off(self, future_off: Future):
-        wheel_velocities_rotate = WheelAngularVelocities()
-        wheel_velocities_rotate.left_wheel_angular_velocity = 0.0
-        wheel_velocities_rotate.right_wheel_angular_velocity = 0.0
+     
+    def after_robot_off(self,future_off:Future):
+        wheel_velocities_rotate=WheelAngularVelocities()
+        wheel_velocities_rotate.left_wheel_angular_velocity=0.0
+        wheel_velocities_rotate.right_wheel_angular_velocity=0.0
         self.wheel_velocity_publisher.publish(wheel_velocities_rotate)
-        response = future_off.result()
-        if response.success == True:
-            self.get_logger().info("Robot off sequence has succeeded!")
+        response=future_off.result()
+        if response.success==True:
+            self.get_logger().info('Robot off sequence has succeeded!')
         else:
-            self.get_logger().info(
-                f"Off sequence response is: {response.success} robot did not turn off successfully!"
-            )
-
+            self.get_logger().info(f'Off sequence response is: {response.success} robot did not turn off successfully!')
 
 def main(args=None):
     try:
         rclpy.init(args=args)
-        omega_operator_controller_node = OmegaOperatorControllerNode()
+        omega_operator_controller_node=OmegaOperatorControllerNode()
         rclpy.spin(omega_operator_controller_node)
     except KeyboardInterrupt:
         pass
     except Exception as e:
         print(e)
-
-
-if __name__ == "__main__":
+if __name__=='__main__':
     main()
-"/rplidar_node/scan/rvis2/initialpose"
+'/rplidar_node/scan/rvis2/initialpose'
